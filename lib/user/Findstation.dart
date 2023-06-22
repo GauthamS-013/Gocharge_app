13f6f2f6091e6ea64587130804a83f2d91fed099
Abhishek M K <abhishekmk9@gmail.com>
2023-06-23 01:17:23 +0530

    find-station-done

--- lib/user/Findstation.dart
+++ lib/user/Findstation.dart
@@ -1,12 +1,17 @@
-import 'package:ev_charging/user/Stationdetails.dart';
-import 'package:ev_charging/user/booknow.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:math' as math;
+
+import 'booknow.dart';
 
 class Station {
   final String? name;
-  final String? location;
+  final double? longitude;
+  final double? lattitude;
+  final String? city;
 
-  Station({this.name, this.location});
+  Station({this.name, this.longitude, this.lattitude,this.city});
 }
 
 class FindStationPage extends StatefulWidget {
@@ -15,29 +20,89 @@ class FindStationPage extends StatefulWidget {
 }
 
 class _FindStationPageState extends State<FindStationPage> {
-  var station=[
-    {
-      'Name': 'Ather Hub',
-      'Location': 'Ramanattukara',
-      'Image': "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },{
-      'Name': 'Ather Station',
-      'Location': 'Kozhikode',
-      'Image': "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },{
-      'Name': 'KSEB evStation',
-      'Location': 'Meenchanda',
-      'Image': "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },{
-      'Name': 'Ather hub',
-      'Location': 'Kozhikode',
-      'Image': "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },{
-      'Name': 'Ather Station',
-      'Location': 'Kozhikode',
-      'Image': "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },
-  ];
+  late List<Station> stations = [];
+  late Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  void _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {
+        _currentPosition = position;
+        _fetchStations();
+      });
+    } catch (e) {
+      print('Failed to get current location: $e');
+    }
+  }
+
+  void _fetchStations() {
+    double range = 20.0; // Range in kilometers
+
+    FirebaseFirestore.instance
+        .collection('ev_stations')
+        .get()
+        .then((QuerySnapshot snapshot) {
+      setState(() {
+        stations = snapshot.docs.map((DocumentSnapshot doc) {
+          final data = doc.data() as Map<String, dynamic>?; // Explicitly cast to Map<String, dynamic>
+          if (data != null && data.containsKey('name') && data.containsKey('longitude') && data.containsKey('lattitude')) {
+            double lattitude = double.tryParse(data['lattitude'].toString()) ?? 0.0;
+            double longitude = double.tryParse(data['longitude'].toString()) ?? 0.0;
+            String city = data['city'];
+            double distanceInKm = _calculateDistance(
+              _currentPosition.latitude,
+              _currentPosition.longitude,
+              lattitude,
+              longitude,
+            );
+
+            if (distanceInKm <= range) {
+              return Station(
+                name: data['name'].toString(),
+                longitude: longitude,
+                lattitude: lattitude,
+                city: data['city'].toString(),
+              );
+            }
+          }
+          return null;
+        }).where((station) => station != null).toList().cast<Station>();
+      });
+    }).catchError((error) {
+      print('Failed to fetch stations: $error');
+    });
+  }
+  double _calculateDistance(
+      double startLattitude,
+      double startLongitude,
+      double endLattitude,
+      double endLongitude,
+      ) {
+    const double earthRadius = 6371; // in kilometers
+
+    double lat1 = startLattitude * (math.pi / 180.0);
+    double lon1 = startLongitude * (math.pi / 180.0);
+    double lat2 = endLattitude * (math.pi / 180.0);
+    double lon2 = endLongitude * (math.pi / 180.0);
+
+    double dLat = lat2 - lat1;
+    double dLon = lon2 - lon1;
+
+    double a = math.sin(dLat / 2) * math.sin(dLat / 2) +
+        math.cos(lat1) * math.cos(lat2) * math.sin(dLon / 2) * math.sin(dLon / 2);
+    double c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a));
+
+    double distanceInKm = earthRadius * c;
+    return distanceInKm;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +120,7 @@ class _FindStationPageState extends State<FindStationPage> {
         ],
       ),
       body: ListView.builder(
-        itemCount: station.length,
+        itemCount: stations.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
@@ -64,24 +129,26 @@ class _FindStationPageState extends State<FindStationPage> {
               width: 90,
               color: Colors.grey,
               child: ListTile(
-                title: Text('Name: ${station[index]["Name"]}',style: TextStyle(fontSize: 18),),
-                subtitle: Text('Location: ${station[index]["Location"]}',style: TextStyle(fontSize: 18),),
-                leading: Container(
-              height: 60,
-              width: 60,
-              decoration: BoxDecoration(
-              image: DecorationImage(image: NetworkImage('https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI='),),
-
-              shape: BoxShape.rectangle,
-              color: Colors.white
-              ),),
+                title: Text(
+                  'Name: ${stations[index].name}',
+                  style: TextStyle(fontSize: 18),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'City: ${stations[index].city}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
                 onTap: () {
                   // TODO: Implement station selection functionality
                   // Perform actions when a station is tapped
                   Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => booknow()),
-                  );
+                      context,
+                      MaterialPageRoute(builder: (context) => booknow()),
+                      );
                 },
               ),
             ),
