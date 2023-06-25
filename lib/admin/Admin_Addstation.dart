5dbb90920c5bba11badf0703be86890db17fda13
Abhishek M K <abhishekmk9@gmail.com>
2023-06-25 18:06:23 +0530

    stations-add-for-owners

--- lib/admin/Admin_Addstation.dart
+++ lib/admin/Admin_Addstation.dart
@@ -1,5 +1,8 @@
 import 'package:ev_charging/admin/Admin_managestation.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class addstation extends StatefulWidget {
   const addstation({Key? key}) : super(key: key);
@@ -11,9 +14,14 @@ class addstation extends StatefulWidget {
 class _addstationState extends State<addstation> {
   GlobalKey<FormState> key4 = GlobalKey<FormState>();
   TextEditingController _stationname = TextEditingController();
-  TextEditingController _location = TextEditingController();
+  TextEditingController _address = TextEditingController();
   TextEditingController _city = TextEditingController();
-  TextEditingController _latilongi = TextEditingController();
+  TextEditingController _state = TextEditingController();
+  LatLng? _selectedLocation;
+
+  final User? user = FirebaseAuth.instance.currentUser;
+  final CollectionReference stationsCollection =
+  FirebaseFirestore.instance.collection('ev_stations');
 
   @override
   Widget build(BuildContext context) {
@@ -31,30 +39,27 @@ class _addstationState extends State<addstation> {
                 height: 18,
               ),
               TextFormField(
-                  controller: _stationname,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Station name',
-                  ),
-                  validator: (n) {
-                    if (n!.isEmpty) {
-                      return "Please enter a name";
-                    }
-                  }),
+                controller: _stationname,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Station name',
+                ),
+                validator: (n) {
+                  if (n!.isEmpty) {
+                    return "Please enter a name";
+                  }
+                  return null;
+                },
+              ),
               SizedBox(
                 height: 18,
               ),
               TextFormField(
-                controller: _location,
+                controller: _address,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Location',
+                  labelText: 'Address',
                 ),
-                // validator: (n) {
-                //   if (n!.isEmpty) {
-                //     return "Please enter a location";
-                //   }
-                // }
               ),
               SizedBox(
                 height: 18,
@@ -65,27 +70,27 @@ class _addstationState extends State<addstation> {
                   border: OutlineInputBorder(),
                   labelText: 'City',
                 ),
-                // validator: (n) {
-                //   if (n!.isEmpty) {
-                //     return "Please enter a city";
-                //   }
-                // }
               ),
               SizedBox(
                 height: 18,
               ),
               TextFormField(
-                controller: _latilongi,
-                keyboardType: TextInputType.number,
+                controller: _state,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Latitude, Longitude',
+                  labelText: 'State',
                 ),
-                // validator: (n) {
-                //   if (n!.isEmpty) {
-                //     return "Please enter latitude, longitude";
-                //   }
-                // }
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              _selectedLocation != null
+                  ? Text(
+                'Selected Location: ${_selectedLocation!.latitude}, ${_selectedLocation!.longitude}',
+              )
+                  : ElevatedButton(
+                onPressed: _selectLocationOnMap,
+                child: Text('Select Location on Map'),
               ),
               SizedBox(
                 height: 23,
@@ -93,12 +98,8 @@ class _addstationState extends State<addstation> {
               ElevatedButton(
                 onPressed: () {
                   bool validate = key4.currentState!.validate();
-                  print(validate);
-                  if (validate == true) {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => admin_managestation()));
+                  if (validate) {
+                    addStationToFirestore();
                   }
                 },
                 style: ButtonStyle(
@@ -118,5 +119,94 @@ class _addstationState extends State<addstation> {
         ),
       ),
     );
+  }
+
+  void _selectLocationOnMap() async {
+    LatLng? location = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MapPicker(),
+      ),
+    );
+
+    if (location != null) {
+      setState(() {
+        _selectedLocation = location;
+      });
+    }
+  }
+
+  void addStationToFirestore() async {
+    try {
+      if (user != null) {
+        String ownerEmail = user!.email!;
+        await stationsCollection.add({
+          'name': _stationname.text,
+          'address': _address.text,
+          'city': _city.text,
+          'state': _state.text,
+          'lattitude': _selectedLocation?.latitude,
+          'longitude': _selectedLocation?.longitude,
+          'type': "49",
+          'ownerEmail': ownerEmail,
+        });
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => admin_managestation()),
+        );
+      }
+    } catch (e) {
+      print('Error adding station to Firestore: $e');
+      // Handle the error as per your requirement
+    }
+  }
+}
+
+class MapPicker extends StatefulWidget {
+  @override
+  _MapPickerState createState() => _MapPickerState();
+}
+
+class _MapPickerState extends State<MapPicker> {
+  GoogleMapController? _mapController;
+  LatLng? _selectedLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Location on Map'),
+      ),
+      body: GoogleMap(
+        onMapCreated: (controller) => _mapController = controller,
+        onTap: _selectLocation,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.7749, -122.4194), // Default initial location
+          zoom: 12,
+        ),
+        markers: _selectedLocation != null
+            ? Set<Marker>.from([
+          Marker(
+            markerId: MarkerId('selectedLocation'),
+            position: _selectedLocation!,
+          ),
+        ])
+            : {},
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_selectedLocation != null) {
+            Navigator.pop(context, _selectedLocation);
+          }
+        },
+        child: Icon(Icons.check),
+      ),
+    );
+  }
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _selectedLocation = position;
+    });
   }
 }
--- lib/admin/Admin_managestation.dart
+++ lib/admin/Admin_managestation.dart
@@ -1,13 +1,15 @@
 import 'package:ev_charging/admin/Admin_Addstation.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../user/Stationdetails.dart';
 
 class Station {
   final String? name;
-  final String? location;
+  final String? city;
 
-  Station({this.name, this.location});
+  Station({this.name, this.city});
 }
 
 class admin_managestation extends StatefulWidget {
@@ -18,38 +20,9 @@ class admin_managestation extends StatefulWidget {
 }
 
 class _admin_managestationState extends State<admin_managestation> {
-  var station = [
-    {
-      'Name': 'Ather Hub',
-      'Location': 'Ramanattukara',
-      'Image':
-          "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },
-    {
-      'Name': 'Ather Station',
-      'Location': 'Kozhikode',
-      'Image':
-          "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },
-    {
-      'Name': 'KSEB evStation',
-      'Location': 'Meenchanda',
-      'Image':
-          "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },
-    {
-      'Name': 'Ather hub',
-      'Location': 'Kozhikode',
-      'Image':
-          "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },
-    {
-      'Name': 'Ather Station',
-      'Location': 'Kozhikode',
-      'Image':
-          "https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI="
-    },
-  ];
+  final User? user = FirebaseAuth.instance.currentUser;
+  final CollectionReference stationsCollection =
+  FirebaseFirestore.instance.collection('ev_stations');
 
   @override
   Widget build(BuildContext context) {
@@ -57,53 +30,79 @@ class _admin_managestationState extends State<admin_managestation> {
       appBar: AppBar(
         title: Text('Manage Stations'),
       ),
-      body: ListView.builder(
-        itemCount: station.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 80,
-              width: 90,
-              color: Colors.grey,
-              child: ListTile(
-                title: Text(
-                  'Name: ${station[index]["Name"]}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                subtitle: Text(
-                  'Location: ${station[index]["Location"]}',
-                  style: TextStyle(fontSize: 18),
-                ),
-                leading: Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI='),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: stationsCollection
+            .where('ownerEmail', isEqualTo: user?.email)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+          List<Station> stations = documents.map((doc) {
+            return Station(
+              name: doc['name'],
+              city: doc['city'],
+            );
+          }).toList();
+
+          return ListView.builder(
+            itemCount: stations.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 80,
+                  width: 90,
+                  color: Colors.grey,
+                  child: ListTile(
+                    title: Text(
+                      'Name: ${stations[index].name}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Text(
+                      'City: ${stations[index].city}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    leading: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://media.istockphoto.com/id/901999846/vector/icon-charging-stations-of-electric-cars.jpg?s=612x612&w=0&k=20&c=mmCSrOptMqHGPugfm-iaHUoJqykLV5vJ24grr9YkcvI=',
+                          ),
+                        ),
+                        shape: BoxShape.rectangle,
+                        color: Colors.white,
                       ),
-                      shape: BoxShape.rectangle,
-                      color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Stationsdetail(),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-                onTap: () {
-                  // TODO: Implement station selection functionality
-                  // Perform actions when a station is tapped
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(builder: (context) => Stationsdetail()),
-                  //);
-                },
-              ),
-            ),
+              );
+            },
           );
         },
       ),
       floatingActionButton: SizedBox(
         width: 130,
         child: FloatingActionButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
           onPressed: () {
             Navigator.push(
               context,
